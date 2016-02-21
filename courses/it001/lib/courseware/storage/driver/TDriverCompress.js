5d0d7dbd7504fe8cfef5e31ebc3dda54a16c1839
peter <peter.hoppe@gmail.com>
2016-02-21 07:05:47 +0100

    TWorksheet: Implemented compression driver for storage layer.

--- courses/it001/lib/courseware/storage/driver/TDriverCompress.js
+++ courses/it001/lib/courseware/storage/driver/TDriverCompress.js
@@ -5,37 +5,119 @@ define
 (
     [
         "dojo/_base/declare",
+        "pieroxy_lz-string/lz-string",
         "./TDriver"
     ],
     function 
     (
         declare,
+        compressor,
         TDriver
     )
     {
+        var kEconomyMin = 20;
+        
         var TDriverCompress;
         var ret;
 
         /**
-         * Insert_explanation_here
+         * The compression driver. (De)Compresses all data to (from) the local 
+         * storage. 
+         * Not all data will be compressed! For each submitted chunk of data 
+         * the driver decides whether to store it compressed or as-is. This is
+         * decided by the amount of storage saved: For each submitted chunk:
+         *     - if compressing can reduce storage size by at least 20% then 
+         *       the data will be stored in compressed format.
+         *     - if savings are less than 20%, then we store uncompressed.
+         * After the compression round the data will be packed into a JS object:
+         *     {
+         *         isCompressed:    boolean     // true if data is compressed
+         *         data:            string      // The data payload
+         *     }
+         * This object will be (de)serialized to/from a JSON record.
          * 
-         * @class       TChangeToClassName
+         * @class       TDriverCompress
          */
         TDriverCompress = 
         {
             Get: function (key)
             {
+                var stor;
+                var record;
                 var ret;
                 
-                ret = this.fStore.getItem (key);
+                stor    = this.fStore.getItem (key);
+                record  = JSON.parse (stor);
+                if (record.isCompressed)
+                {
+                    ret = compressor.decompressFromBase64 (record.data);
+                }
+                else
+                {
+                    ret = record.data;
+                }
+                
+                this._LogStats ("TDriverCompress::Get", record.isCompressed, ret, record.data);
                 
                 return ret;
             },
         
             Set: function (key, data)
             {
-                this.fStore.setItem (key, data);
+                var compr;
+                var lPerc;
+                var economy;
+                var record;
+                var stor;
+                
+                compr   = compressor.compressToBase64 (data);                   /* [10] */
+                if (compr.length >= 1)
+                {
+                    lPerc   = 100 * compr.length / data.length;
+                    economy = 100 - lPerc;
+                    if (economy >= kEconomyMin)
+                    {
+                        record =
+                        {
+                            isCompressed:   true,
+                            data:           compr
+                        };
+                    }
+                    else
+                    {
+                        record =
+                        {
+                            isCompressed:   false,
+                            data:           data
+                        };
+                    }
+                }
+                else
+                {
+                    record =
+                    {
+                        isCompressed:   false,
+                        data:           ""
+                    }
+                }
+                stor = JSON.stringify (record);
+                this.fStore.setItem (key, stor);
+                this._LogStats ("TDriverCompress::Set", record.isCompressed, data, compr);
             },
+            
+            _LogStats: function (ctx, isCompressed, data, compr)
+            {
+                var lData;
+                var lCompr;
+                var lPerc;
+                var flagStr;
+                
+                lData   = data.length;
+                lCompr  = compr.length;
+                lPerc   = 100 * lCompr / lData;
+                flagStr = isCompressed ? "true " : "false";
+                console.log (ctx + "[compressed:" + flagStr + "]: lenData=" + lData + ", lenCompressed=" + lCompr + " (" + (Math.round (10 * lPerc)/10) + "%)")
+            }
         };
     
         ret = declare ("TDriverCompress", [TDriver], TDriverCompress);
@@ -43,3 +125,9 @@ define
         return ret;
     }
 );
+
+/*
+
+ [10]: We compress to (decompress from) a base64 string as this uses a safe character set.
+
+ */
