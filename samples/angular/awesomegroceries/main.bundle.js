34c40a7cbe2929be02beea30c533e4166a8a3877
Peter Hoppe <peter@ph-desktop.ph.mphshori.net>
2018-02-28 16:54:32 +0100

    fixed base-href

--- samples/angular/awesomegroceries/main.bundle.js
+++ samples/angular/awesomegroceries/main.bundle.js
@@ -161,6 +161,7 @@ var AppModule = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__pages_tlanding_tlanding_component__ = __webpack_require__("../../../../../src/app/components/pages/tlanding/tlanding.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__pages_tdetails_tdetails_component__ = __webpack_require__("../../../../../src/app/components/pages/tdetails/tdetails.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_tcart_tcart_component__ = __webpack_require__("../../../../../src/app/components/pages/tcart/tcart.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pages_tfour_oh_four_tfour_oh_four_component__ = __webpack_require__("../../../../../src/app/components/pages/tfour-oh-four/tfour-oh-four.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -172,11 +173,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
 var kRoutes = [
     { path: 'landing', component: __WEBPACK_IMPORTED_MODULE_2__pages_tlanding_tlanding_component__["a" /* TLandingComponent */] },
     { path: 'details/:id', component: __WEBPACK_IMPORTED_MODULE_3__pages_tdetails_tdetails_component__["a" /* TDetailsComponent */] },
     { path: 'shopping-cart', component: __WEBPACK_IMPORTED_MODULE_4__pages_tcart_tcart_component__["a" /* TCartComponent */] },
-    { path: '**', redirectTo: '/landing', pathMatch: 'full' }
+    { path: '', redirectTo: '/landing', pathMatch: 'full' },
+    { path: '**', component: __WEBPACK_IMPORTED_MODULE_5__pages_tfour_oh_four_tfour_oh_four_component__["a" /* TFourOhFourComponent */] }
 ];
 var AppRouterModule = /** @class */ (function () {
     function AppRouterModule() {
@@ -647,7 +650,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/components/widgets/tlogo/tlogo.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<img src=\"http://ustegrew.github.io/samples/angular/awesomegroceries/assets/branding/logo.png\">\n"
+module.exports = "<img src=\"/assets/branding/logo.png\">\n"
 
 /***/ }),
 
